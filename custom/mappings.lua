75ce93a229d27eda3c9317224941d0f2e4f16831
MilkJug1 <michael.sagan.crawford@gmail.com>
2023-04-22 22:08:54 -0400

    Added nvim conf

---
+++ custom/mappings.lua
@@ -0,0 +1,12 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = {":", "<cmd>FineCmdline<CR>", opts = { nowait = true}},
+  },
+}
+
+-- more keybinds!
+
+return M
---
+++ custom/plugins.lua
@@ -0,0 +1,96 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+
+ -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      overrides.mason, 
+      ensure_installed = {
+        "rust_analyzer",
+        "cpplint",
+        "cpptools",
+        "clangd"
+      }
+    }
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  {
+     "folke/noice.nvim",
+  config = function()
+    require "noice".setup({
+      })
+  end,
+  dependencies = {
+      "MunifTanjim/nui.nvim"
+    },
+  },
+
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+
+  {
+    'saecki/crates.nvim',
+    ft = {"rust", "toml"},
+    config = function(_, opts)
+      local crates = require('crates')
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+
+  -- To make a plugin not be loaded
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
+  -- },
+
+}
+
+return plugins
