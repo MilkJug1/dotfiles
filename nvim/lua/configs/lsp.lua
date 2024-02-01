f233b733c246cb236596efaed0ddcfdc6e8b3b0d
MilkJug1 <michael.sagan.crawford@gmail.com>
2024-01-31 22:18:37 -0500

    Stuff for LSP

--- nvim/lua/configs/lsp.lua
+++ nvim/lua/configs/lsp.lua
@@ -1,16 +1,17 @@
 local on_attach = function(_, bufnr)
-  local bufmap = function(keys, func)
-    vim.keymap.set('n', keys, func, { buffer = bufnr })
-  end
-  --
- -- bufmap('K', vim.lsp.buf.hover)
+	local bufmap = function(keys, func)
+		vim.keymap.set('n', keys, func, { buffer = bufnr })
+	end
+	--
+	-- bufmap('K', vim.lsp.buf.hover)
 
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, {})
+	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+		vim.lsp.buf.format()
+	end, {})
 end
 
 
+local MY_FQBN = "arduino:avr:mega"
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
@@ -38,13 +39,13 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 require('mason').setup()
 require('mason-lspconfig').setup_handlers({
 	function(server_name)
-		require ('lspconfig')[server_name].setup{
+		require('lspconfig')[server_name].setup {
 			on_attach = on_attach,
 			capabilities = capabilities
 		}
 	end,
 
-	["lua_ls"] = function ()
+	["lua_ls"] = function()
 		require('neodev').setup()
 		require('lspconfig').lua_ls.setup {
 			on_attach = on_attach,
@@ -56,7 +57,6 @@ require('mason-lspconfig').setup_handlers({
 				},
 			}
 		}
-
 	end,
 
 	["clangd"] = function()
@@ -67,6 +67,16 @@ require('mason-lspconfig').setup_handlers({
 				'clangd',
 				'--offset-encoding=utf-16',
 			},
+		}
+	end,
+
+	["arduino-language-server"] = function()
+		require('lspconfig').arduino_language_server.setup {
+			on_attach = on_attach,
+			capabilities = capabilities,
+			cmd = {
+				'arduino-language-server'
+			}
 		}
 	end
 })
