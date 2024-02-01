9767a64435d550796a68454b89ddfaebdf55cccb
MilkJug1 <michael.sagan.crawford@gmail.com>
2024-01-31 22:22:29 -0500

    Added Neotree

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -107,6 +107,36 @@ return {
 		}
 	},
 
+	-- {
+	-- 	"nvim-neo-tree/neo-tree.nvim",
+	-- 	branch = "v3.x",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	-- 		"MunifTanjim/nui.nvim",
+	-- 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	-- 	},
+	-- 	config = function()
+	-- 		require('neo-tree').setup({
+	-- 			source_selector = {
+	-- 				winbar = true,
+	-- 				statusline = false,
+	--
+	-- 			},
+	--
+	-- 			sources = {
+	-- 				"filesystem",
+	-- 				"document_symbols",
+	-- 				"buffers",
+	-- 			},
+	--
+	-- 			window = {
+	-- 				position = 'right',
+	-- 			},
+	-- 		})
+	-- 	end
+	-- },
+
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
