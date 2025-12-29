ed3322457de50de4311e4b7f74744da854f6b692
MilkJug1 <michael.sagan.crawford@gmail.com>
2025-12-29 14:39:12 -0500

    Altered some keybinds, had to update lsp-config for newer neovim version, changed formatting for dashboard

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -148,13 +148,14 @@ return {
         ---@type Flash.Config
         opts = {},
         -- stylua: ignore
-        -- keys = {
-        --   { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-        --   { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-        --   { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-        --   { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        --   { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-        -- },
+        keys = {
+          { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+          { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+          { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+          { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+          { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        }
+
     },
 
     {
@@ -199,10 +200,10 @@ return {
             vim.keymap.set("n", "ta", function() harpoon:list():add() end)
             vim.keymap.set("n", "to", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-            vim.keymap.set("n", "t1", function() harpoon:list():select(1) end, { desc = "Jump to Harpoon Selection 1"})
-            vim.keymap.set("n", "t2", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "t3", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "t4", function() harpoon:list():select(4) end)
+            vim.keymap.set("n", "<M-q>", function() harpoon:list():select(1) end, { desc = "Jump to Harpoon Selection 1"})
+            vim.keymap.set("n", "<M-w>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<M-e>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
 
             -- Toggle previous & next buffers stored within Harpoon list
             vim.keymap.set("n", "tn", function() harpoon:list():prev() end)
@@ -239,4 +240,15 @@ return {
     {
         "jbyuki/venn.nvim",
     },
+
+    -- {
+    --     "code-biscuits/nvim-biscuits",
+    --     dependencies = {
+    --         'nvim-treesitter/nvim-treesitter',
+    --     },
+    --     config = function ()
+    --         require('nvim-biscuits').setup()
+    --     end
+    -- }
+
 }
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,67 +1,97 @@
-vim.lsp.enable({ "zls", "clangd", "lua_ls", "pyright", "cmake", "bashls", "marksman", })
+vim.lsp.enable({ "zls", "clangd", "lua_ls", "basedpyright", "cmake", "bashls", "marksman" })
 
 -- Seemingly Mason-lspconfig stuff is not working no more, heres is a semi-temp workaround till i can make a better config
-vim.lsp.config['luals'] = {
-    -- Command and arguments to start the server.
-    cmd = { 'lua-language-server' },
-    -- Filetypes to automatically attach to.
-    filetypes = { 'lua' },
-    -- Sets the "root directory" to the parent directory of the file in the
-    -- current buffer that contains either a ".luarc.json" or a
-    -- ".luarc.jsonc" file. Files that share a root directory will reuse
-    -- the connection to the same LSP server.
-    -- Nested lists indicate equal priority, see |vim.lsp.Config|.
-    root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-    -- Specific settings to send to the server. The schema for this is
-    -- defined by the server. For example the schema for lua-language-server
-    -- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
-    settings = {
-        Lua = {
-            runtime = {
-                version = 'LuaJIT',
-            }
-        }
-    }
+vim.lsp.config["luals"] = {
+	-- Command and arguments to start the server.
+	cmd = { "lua-language-server" },
+	-- Filetypes to automatically attach to.
+	filetypes = { "lua" },
+	-- Sets the "root directory" to the parent directory of the file in the
+	-- current buffer that contains either a ".luarc.json" or a
+	-- ".luarc.jsonc" file. Files that share a root directory will reuse
+	-- the connection to the same LSP server.
+	-- Nested lists indicate equal priority, see |vim.lsp.Config|.
+	root_markers = { { ".luarc.json", ".luarc.jsonc" }, ".git" },
+	-- Specific settings to send to the server. The schema for this is
+	-- defined by the server. For example the schema for lua-language-server
+	-- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+	settings = {
+		Lua = {
+			hint = { enable = true },
+			runtime = {
+				version = "LuaJIT",
+			},
+		},
+	},
+}
+
+vim.lsp.config("clangd", {
+	settings = {
+		clangd = {
+			InlayHints = {
+				Enabled = true,
+				ParameterNames = true,
+				DeducedTypes = true,
+				Designators = true,
+			},
+			fallbackFlags = { "-std=c++20" },
+		},
+	},
+})
+
+vim.lsp.config["zls"] = {
+	cmd = { "zls" },
+
+	filetypes = { "zig", "zir" },
+	settings = {
+		zls = {
+			enable_inlay_hints = true,
+			inlay_hints_show_builtin = true,
+			inlay_hints_exclude_single_argument = true,
+			inlay_hints_hide_redundant_param_names = false,
+			inlay_hints_hide_redundant_param_names_last_token = false,
+		},
+	},
 }
 
 -- Add LSP related plugins here, configuration goes on top.
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-    {
-        "onsails/lspkind.nvim"
-    },
+	{
+		"onsails/lspkind.nvim",
+	},
 
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd", "zls", "cmake", "marksman", "basedpyright"}
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            -- "hrsh7th/cmp-nvim-lsp",
-            'saghen/blink.cmp',
-        },
-    },
-    {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-            library = {
-                -- See the configuration section for more details
-                -- Load luvit types when the `vim.uv` word is found
-                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-            },
-        },
-    },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd", "zls", "cmake", "marksman", "basedpyright" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			-- "hrsh7th/cmp-nvim-lsp",
+			"saghen/blink.cmp",
+		},
+	},
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
 }
